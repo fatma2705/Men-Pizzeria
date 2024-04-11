34918513f31cafb830d934530e62d481123c3ffa
fatma2705 <arafatma2003@gmail.com>
2024-04-11 17:26:18 +0200

    update project

--- src/main/java/it/prova/menupizzeria/test/TestMenuPizzeria.java
+++ src/main/java/it/prova/menupizzeria/test/TestMenuPizzeria.java
@@ -2,6 +2,7 @@ package it.prova.menupizzeria.test;
 
 import java.util.List;
 
+import it.prova.menupizzeria.dao.EntityManagerUtil;
 import it.prova.menupizzeria.model.Pizza;
 import it.prova.menupizzeria.service.IngredienteService;
 import it.prova.menupizzeria.service.MyServiceFactory;
@@ -10,33 +11,84 @@ import it.prova.menupizzeria.service.PizzaService;
 public class TestMenuPizzeria {
 
 	public static void main(String[] args) {
-		
-		PizzaService pizzaServiceInstance = MyServiceFactory.getpizzaServiceInstance();
-		IngredienteService ingredienteServiceInstance = MyServiceFactory.getIngredienteServiceInstance(); 
 
-		
-		getAllPizze(pizzaServiceInstance);
-		
-		
-		
+		System.out.println(" ---------- Inizio programma -------------");
+
+		PizzaService pizzaServiceInstance = MyServiceFactory.getpizzaServiceInstance();
+		IngredienteService ingredienteServiceInstance = MyServiceFactory.getIngredienteServiceInstance();
+		Pizza pizza = new Pizza(15L, 6, "margherita", false);
+
+		try {
+
+			getAllPizze(pizzaServiceInstance);
+
+			inserisciPizza(pizzaServiceInstance, pizza);
+
+			aggiornaPizza(pizzaServiceInstance, pizza);
+
+			rimuoviPizza(pizzaServiceInstance, pizza);
+
+		} catch (Throwable e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			EntityManagerUtil.shutdown();
+		}
+
+		System.out.println(" --------------Fine programma pizza ----------------");
 
 	}
-	
-	
+
 	public static void getAllPizze(PizzaService pizzaInstance) {
-		
+
 		System.out.println(" ----------- Stampa tutte le pizze presenti nel database -------------");
-		
+
 		try {
-			List<Pizza> pizza =pizzaInstance.listAll();
+			List<Pizza> pizza = pizzaInstance.listAll();
 			for (Pizza i : pizza) {
 				System.out.println(i.toString());
-				}
-			
-			
+			}
+
 		} catch (Exception e) {
-	        e.printStackTrace();
-	    }
+			e.printStackTrace();
+		}
 	}
 
+	public static void inserisciPizza(PizzaService pizzaInstance, Pizza pizza) {
+		System.out.println(" ----------- Inserisci pizza -------------");
+
+		try {
+			pizzaInstance.inserieciElemento(pizza);
+			System.out.println(" ----------- Pizza inserita con successo ----");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void aggiornaPizza(PizzaService pizzaInstance, Pizza pizza) {
+		System.out.println(" -------------- Aggiorna Pizza  -----------");
+
+		try {
+			pizzaInstance.aggiorna(pizza);
+			System.out.println(" ----------- Pizza aggiornata con successo ----");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void rimuoviPizza(PizzaService pizzaInstance, Pizza pizza) {
+		System.out.println(" -----------------------Rimuovi Pizza ----------------");
+
+		try {
+			pizzaInstance.rimuovi(pizza);
+			System.out.println(" ----------- Pizza rimossa con successo ----");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
