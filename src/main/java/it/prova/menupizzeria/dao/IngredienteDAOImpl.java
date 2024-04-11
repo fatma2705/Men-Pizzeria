16140658abc45b140281068b91012d00f56e3d7e
fatma2705 <arafatma2003@gmail.com>
2024-04-11 12:09:53 +0200

    modifca classi PizzaDAOImpl & IngredienteDAOImpl

--- src/main/java/it/prova/menupizzeria/dao/IngredienteDAOImpl.java
+++ src/main/java/it/prova/menupizzeria/dao/IngredienteDAOImpl.java
@@ -6,41 +6,51 @@ import it.prova.menupizzeria.model.Ingrediente;
 import jakarta.persistence.EntityManager;
 
 public class IngredienteDAOImpl implements IngredienteDAO{
+	
+	private EntityManager entityManager;
+	
+	
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
 
 	@Override
 	public List<Ingrediente> list() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return entityManager.createQuery("from ingrediente",Ingrediente.class).getResultList();
 	}
 
 	@Override
 	public Ingrediente get(Long id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return entityManager.find(Ingrediente.class, id);
 	}
 
 	@Override
-	public void update(Ingrediente o) throws Exception {
-		// TODO Auto-generated method stub
+	public void update(Ingrediente ingredienteInstance) throws Exception {
+		if (ingredienteInstance == null) {
+			throw new Exception("Impossibile eseguire l' update nel DB. Input non valido");
+		} ingredienteInstance = entityManager.merge(ingredienteInstance);
 		
 	}
 
 	@Override
-	public void insert(Ingrediente o) throws Exception {
-		// TODO Auto-generated method stub
-		
+	public void insert(Ingrediente ingredienteInstance) throws Exception {
+		if (ingredienteInstance == null) {
+			throw new Exception("Errore valore input");
+		} 
+		entityManager.persist(ingredienteInstance);
 	}
 
 	@Override
-	public void delete(Ingrediente o) throws Exception {
-		// TODO Auto-generated method stub
+	public void delete(Ingrediente ingredienteInstance) throws Exception {
+		if (ingredienteInstance == null) {
+			throw new Exception("Impossibile eseguire la delete nel DB. Input non valido");
+		} entityManager.remove(entityManager.merge(ingredienteInstance));
 		
+	
 	}
 
-	@Override
-	public void setEntityManager(EntityManager entityManager) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 
 }
--- src/main/java/it/prova/menupizzeria/dao/PizzaDAOImpl.java
+++ src/main/java/it/prova/menupizzeria/dao/PizzaDAOImpl.java
@@ -6,41 +6,49 @@ import it.prova.menupizzeria.model.Pizza;
 import jakarta.persistence.EntityManager;
 
 public class PizzaDAOImpl implements PizzaDAO{
+	
+	
+	
+	private EntityManager entityManager;
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
 
 	@Override
 	public List<Pizza> list() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		return entityManager.createQuery("from pizza", Pizza.class).getResultList();	}
 
 	@Override
 	public Pizza get(Long id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return entityManager.find(Pizza.class, id);
 	}
 
 	@Override
-	public void update(Pizza o) throws Exception {
-		// TODO Auto-generated method stub
+	public void update(Pizza pizzaInstance) throws Exception {
+		if (pizzaInstance == null) {
+			throw new Exception("Impossibile eseguire l' update nel DB. Input non valido");
+		} pizzaInstance = entityManager.merge(pizzaInstance);
+			
+	}
+
+	@Override
+	public void insert(Pizza pizzaInstance) throws Exception {
+		if (pizzaInstance == null) {
+			throw new Exception("Errore valore input");
+		} 
+		entityManager.persist(pizzaInstance);
 		
 	}
 
 	@Override
-	public void insert(Pizza o) throws Exception {
-		// TODO Auto-generated method stub
+	public void delete(Pizza pizzaInstance) throws Exception {
+		if (pizzaInstance == null) {
+			throw new Exception("Impossibile eseguire la delete nel DB. Input non valido");
+		} entityManager.remove(entityManager.merge(pizzaInstance));
 		
 	}
 
-	@Override
-	public void delete(Pizza o) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setEntityManager(EntityManager entityManager) {
-		// TODO Auto-generated method stub
-		
-	}
 
 }
