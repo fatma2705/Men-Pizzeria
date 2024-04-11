8bcb964fd416430b14057d2c7551c07a170b1ece
fatma2705 <arafatma2003@gmail.com>
2024-04-11 10:26:01 +0200

    creazione classi PizzaDAO & IngredienteDAO

---
+++ src/main/java/it/prova/menupizzeria/dao/IngredienteDAO.java
@@ -0,0 +1,7 @@
+package it.prova.menupizzeria.dao;
+
+import it.prova.menupizzeria.model.Ingrediente;
+
+public interface IngredienteDAO extends IBaseDAO<Ingrediente> {
+
+}
---
+++ src/main/java/it/prova/menupizzeria/dao/PizzaDAO.java
@@ -0,0 +1,9 @@
+package it.prova.menupizzeria.dao;
+
+import it.prova.menupizzeria.model.Pizza;
+
+public interface PizzaDAO extends IBaseDAO<Pizza>{
+	
+	
+
+}
